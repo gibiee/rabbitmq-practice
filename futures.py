d7fcf37b6f3b5292b0e6c8640a052e4249d6dd90
gibiee <gibiee@naver.com>
2025-01-03 16:52:31 +0000

    test with sleep

--- futures.py
+++ futures.py
@@ -11,10 +11,21 @@ def calculate(iter=100000000) :
     print(id(data), data)
 
     total = 0
-    for i in range(iter) : # 1억번 반복 : 약 5초 소요
+    for i in range(iter): # 1억번 반복 : 약 5초 소요
         total += i
 
-    print(f"[PID {os.getpid()}] 실행 완료! {time.time()-start_time:.2f}s")
+    print(f"[PID {os.getpid()}] calculate() 실행 완료! {time.time()-start_time:.2f}s")
+    return total
+
+def calculate_sleep(iter=5) :
+    start_time = time.time()
+
+    total = 0
+    for i in range(iter):
+        total += i
+        time.sleep(1)
+
+    print(f"[PID {os.getpid()}] calculate_sleep() 실행 완료! {time.time()-start_time:.2f}s")
     return total
 
 if __name__ == '__main__':
@@ -25,12 +36,18 @@ if __name__ == '__main__':
     FOR_ITER = 10
 
     jobs = []
-    # executor = concurrent.futures.ThreadPoolExecutor(max_workers=WORKERS)
-    executor = concurrent.futures.ProcessPoolExecutor(max_workers=WORKERS)
-    for i in range(FOR_ITER):
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=30)
+    # executor = concurrent.futures.ProcessPoolExecutor(max_workers=WORKERS)
+    for i in range(4):
         job = executor.submit(calculate, iter=100000000)
-        print('-----', type(job), job)
         jobs.append(job)
+        print('-----', type(job), job)
+        time.sleep(0.1)
+
+    for i in range(10):
+        job = executor.submit(calculate_sleep, iter=5)
+        jobs.append(job)
+        print('-----', type(job), job)
         time.sleep(0.1)
     
     for job in jobs :
